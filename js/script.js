5d7f72ad57487427c05409f32d10a613613500ca
Zain Ali <zain.ali@kidsway.app>
2020-07-15 22:15:43 +0500

    update js html and styles

--- js/script.js
+++ js/script.js
@@ -1,41 +1,29 @@
-
- function len(obj) {
-        var size = 0,
-            key;
-        for (key in obj) {
-            if (obj.hasOwnProperty(key)) size++;
-        }
-        return size;
-    };
-
-    function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
-    }
-    var a = [];
-    var unique = [];
-    var covers = '';
-    var cover = '';
-    var fields=[];
-     var $container = $('.images');
-   $(window).load(function(){
-        
-       
-            $container.isotope({
-            filter: '*',
-            animationOptions: {
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
+var categories = [];
+var unique = [];
+var covers = '';
+var cover = '';
+var fields = [];
+var $container = $('.images');
+window.onload = () => {
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
             duration: 750,
             easing: 'linear',
             queue: false
-            }
-          
-            });
-             });
-$(document).ready(function() {
-    //scrolling nav change
-    $(".nav-link:first").addClass('active');
-    $(window).scroll(function() {
+        }
 
-    var nav = $('nav');
+    });
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    //scrolling nav change
+    document.querySelector(".nav-link").classList.add("active");
+    window.addEventListener('scroll', function() {
+        var nav = $('nav');
         var top = 100;
         var scrolled_val = $(window).scrollTop().valueOf();
         if (scrolled_val < top) {
@@ -44,82 +32,114 @@ $(document).ready(function() {
             nav.addClass('bg-primary');
         }
     });
- $(".nav-link").on('click', function(event) {
-    $(".nav-link").removeClass('active');
-    $(this).addClass('active');
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+    var navlink = [];
+    navlink.push(...document.getElementsByClassName("nav-link"));
 
-      // Store hash
-      var hash = this.hash;
+    navlink.forEach(e => {
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+        e.addEventListener('click', function(event) {
+            const active = document.querySelector('.active');
+            if (active) {
+                active.classList.remove('active');
+            }
+            this.classList.add('active');
 
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+                // Store hash
+                var hash = this.hash;
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                var offset = document.querySelector(hash).offsetTop;
+                $('html, body').animate({
+                    scrollTop: offset
+                }, 800, function() {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                });
+            } // End if
+        });
+
+        //         
+    });
+
     $.get({
         url: "https://api.behance.net/v2/users/zainalidesigns/projects?api_key=NoXD4d6Xe6F4jZpDSiTxkf9NBWwmr2zb",
         dataType: 'jsonp',
-        success: function(data) {
+        success: (data) => {
+            console.log(data);
+            data.projects.forEach((element, index) => {
+                var name = element.name;
+                fields = element.fields.toString().split(' ').join('-').split('/').join('-').split(',');
+                categories.push(fields);
+                fields = fields.join(', ');
 
-            var t = len(data.projects);
-            for (i = 0; i < t; i++) {
-                var name = data.projects[i].name;
-                
-                fields = data.projects[i].fields.toString().split(' ').join('-').split('/').join('-').split(',');
-                a.push(fields);
+                cover = `<div class='col-md-4 ` + fields + ` text-center'>
+                                    <a href=" + data.projects[i].url + ">
+                                        <img src=` + element.covers['404'] + ` class='img-fluid' >
+                                        <p class='my-3'>` + name + `</p>
+                                    </a>
+                            </div>`;
+                cover = $(cover);
 
-                cover= "<div class='col-md-4 text-center'><a href="+data.projects[i].url+"><img src=" + data.projects[i].covers['404'] + " class='img-fluid' ><p class='my-3'>" + name + "</p></a></div>";
-                cover=$(cover);
-                $.each(fields, function( i, value ) {
-                    cover=cover.addClass(value);
-                });
-                
                 $(".images").append(cover);
-    
-           
-              }
-             a= a.toString().split(',');
-            var b = '';
-            unique = a.filter(onlyUnique);
-            for (i = 0; i < unique.length; i++) {
-                b = "<div data-filter='."+unique[i]+"'  class='btn btn-outline-primary my-1 mx-1 col-sm-12 col-md-auto filter '>" + unique[i].toString().split('-').join(' ') + "</div>";
-                $(".filters").append(b);
-            }
+            });
 
-            
-           
-          
+            categories = categories.toString().split(',');
+            var nodes = '';
+            unique = categories.filter(onlyUnique);
+            unique.forEach(e => {
+                const string = e.toString().split('-').join(' ')
+                nodes = `<div data-filter='.` + e + `'  class='btn btn-outline-primary my-1 mx-1 col-sm-12 col-md-auto filter '>` +
+                    string +
+                    `</div>`;
+                $(".filters").append(nodes);
+            });
+
+
+
+
+        },
+        complete: () => {
+            var filters = [];
+            filters.push(...document.getElementsByClassName('filter'));
+            // console.log(filters);
+            filters.forEach(e => {
+                e.addEventListener('click', (event) => {
+                    event.preventDefault();
+
+                    var selector = $(event.target).attr('data-filter');
+
+                    $container.isotope({
+                        filter: selector,
+                        animationOptions: {
+                            duration: 750,
+                            easing: 'linear',
+                            queue: false
+                        }
+                    });
+                    return false;
+                });
+
+                e.onclick = () => {
+
+                    const active = document.querySelector('.btn-primary');
+                    if (active) {
+                        active.classList.toggle('btn-outline-primary');
+                        active.classList.toggle('btn-primary');
+                    }
+                    event.target.classList.toggle('btn-primary');
+                    event.target.classList.toggle('btn-outline-primary');
+                };
+            });
         }
     });
- 
-     $(document).on('click','.filter',function(){
-              $(this).siblings().removeClass("btn-primary").addClass("btn-outline-primary");
-              $(this).removeClass("btn-outline-primary").addClass("btn-primary");
-               var selector = $(this).attr('data-filter');
-              
-            $container.isotope({
-                filter: selector,
-                 animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-                }
-            });
-            
-            return false;
-            });
-     // $(".filter").addClass("btn-outline-primary").removeClass("btn-primary");
+
+    // $('.filter').on('click', function(event) {
+    //    
+    // });
 
 });
-
